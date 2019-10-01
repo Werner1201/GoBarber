6aff213fb78935af8ee96f9b48725f3d8320671d
Werner1201 <werner.romling@unigranrio.br>
2019-10-01 16:55:42 -0300

    configurando o middleware de autenticacao de updade de usuario

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,14 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// Como esta definido depois dos metodos acima so vale para os metodos apos
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
